5785076e9ce546f846673ad45bffc118481b125d
Mikko Himanka <mikko.himanka@protonmail.com>
2019-11-14 18:37:01 +0200

    heroku

---
+++ build/precache-manifest.7c3ea7c02d1139209a0340dc6c2b5251.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a25e361fa0c08ca4d868b31cdf373b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7ffaa2283cce6487358",
+    "url": "/static/js/2.25f7c266.chunk.js"
+  },
+  {
+    "revision": "c1cae98d31fddd5d034b",
+    "url": "/static/js/main.597fb9da.chunk.js"
+  },
+  {
+    "revision": "4e646f0e581801a589fc",
+    "url": "/static/js/runtime-main.7767e197.js"
+  }
+]);

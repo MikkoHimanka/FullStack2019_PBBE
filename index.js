891513c3fa3063b3979270b7426122dc8d79ff38
Mikko Himanka <mikko.himanka@protonmail.com>
2019-11-14 16:28:42 +0200

    heroku

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const port = process.env.PORT || 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
